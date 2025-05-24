d9298e76ce014c837718c26c3bffb9fbe20ef061
P1rat3R00t <piratebooty309@gmail.com>
2025-05-24 02:19:24 +0200

    Update and rename diskcryptor_malicious_stub.cpp to nsfw.cpp

--- core/diskcryptor_malicious_stub.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <windows.h>
-#include <fstream>
-
-extern "C" __declspec(dllexport) void InitDisk() {
-    // Simulate credential dump or disk wipe
-    MessageBoxA(0, "DiskCryptor Payload Executed", "PWNED", MB_OK);
-    // You can replace this with WriteFile() to \\.\PhysicalDrive0
-}
